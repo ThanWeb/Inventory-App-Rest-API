65126a1467f529d6958c4d0ec2dcb61126c3da58
Hans <classsickpeople@gmail.com>
2024-01-17 13:11:59 +0800

    Update product controller to only delete product with false isDeleted attribute and only update product with different name

--- app/controllers/productController.ts
+++ app/controllers/productController.ts
@@ -56,11 +56,19 @@ async function updateProduct (req: typeof Request, res: typeof Response): Promis
     const { id, name, capitalPrice, sellPrice, stock, unit }: { id: number, name: string, capitalPrice: number, sellPrice: number, stock: number, unit: string } = req.body
 
     const isExist = await checkProductExistById(id)
+    const sameName = await checkProductExistByName(name)
 
     if (isExist === null) {
       return res.status(404).json({
         error: true,
         message: 'Product Not Found'
+      })
+    }
+
+    if (sameName !== null) {
+      return res.status(400).json({
+        error: true,
+        message: 'Product With Same Name Already Exist'
       })
     }
 
@@ -85,7 +93,7 @@ async function deleteProduct (req: typeof Request, res: typeof Response): Promis
 
     const isExist = await checkProductExistById(id)
 
-    if (isExist === null) {
+    if (isExist === null || isExist.isDeleted === true) {
       return res.status(404).json({
         error: true,
         message: 'Product Not Found'
