627328c7c64fe783aa52215af6a1648e8fbee9ec
Hans <classsickpeople@gmail.com>
2024-01-20 17:03:17 +0800

    Update cors config

--- app/index.ts
+++ app/index.ts
@@ -9,8 +9,13 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT ?? 8000
 
+const corsOptions = {
+  origin: [process.env.CLIENT_URL ?? ''],
+  credentials: true
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
-app.use(cors())
 app.use('/', router)
 app.use(cookieParser())
 
