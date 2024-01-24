783f78c7210c8771e86a9812706d3c161e271ed0
Hans <classsickpeople@gmail.com>
2024-01-24 21:27:27 +0800

    Install and use body parser

--- app/index.ts
+++ app/index.ts
@@ -3,6 +3,7 @@ const cors = require('cors')
 const dotenv = require('dotenv')
 const cookieParser = require('cookie-parser')
 const router = require('./routes')
+const bodyParser = require('body-parser')
 
 dotenv.config()
 
@@ -20,6 +21,8 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.use(cookieParser())
 app.set('trust proxy', true)
+app.use(bodyParser.json({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   console.log(`\nApp Running on http://localhost:${port}`)
