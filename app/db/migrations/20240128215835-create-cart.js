9b1cfa9689f8ce559063bf2c52781d11a41257f5
Hans <classsickpeople@gmail.com>
2024-01-28 22:01:48 +0800

    Create migration and model for cart

---
+++ app/db/migrations/20240128215835-create-cart.js
@@ -0,0 +1,25 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Carts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      productId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      transcationId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Carts');
+  }
+};
