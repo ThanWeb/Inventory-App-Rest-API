7a6de0489ff4e5b95661d60115fc0acef2eaa52a
Hans <classsickpeople@gmail.com>
2024-04-18 09:30:36 +0800

    Install laravel

--- index.js
+++
@@ -1,6 +0,0 @@
-const data = '{"title":"Mr","fullName":"Hans","familyName":"","dateOfBirth":"2017-01-10T05:17:34.513Z","nationality":"Indonesia","passportNumber":"13131313","expiredUntil":"2024-01-25T05:17:34.513Z"},{"title":"Mr","fullName":"b","familyName":"","dateOfBirth":"2024-01-25T05:17:34.513Z","nationality":"b","passportNumber":"13131313131","expiredUntil":"2024-01-25T05:17:34.513Z"}'
-
-const data2 = '{"title":"Mr","fullName":"Hans","familyName":"","dateOfBirth":"2017-01-10T05:17:34.513Z","nationality":"Indonesia","passportNumber":"13131313","expiredUntil":"2024-01-25T05:17:34.513Z"}'
-
-console.log(data.split('}'))
-console.log(data2.split('}'))
