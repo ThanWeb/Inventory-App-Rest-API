df34af38e1045d00107061a2d8e381b73edb50a6
Hans <classsickpeople@gmail.com>
2024-05-01 18:44:34 +0800

    create auth controller, update user model and create router for authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use App\Models\User;
+use App\Http\Requests\UserLoginRequest;
+use App\Http\Requests\UserRegisterRequest;
+use App\Http\Resources\UserResource;
+
+class AuthController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login','register']]);
+    }
+
+    public function login(UserLoginRequest $request)
+    {
+        $request->validated();
+        $credentials = $request->only('username', 'password');
+
+        $token = Auth::attempt($credentials);
+
+        if (!$token) {
+            throw new HttpResponseException(response([
+                'errors' => [
+                    'auth' => [
+                            'Unauthorized.'
+                        ]
+                    ]
+            ], 403));
+
+        }
+
+        $user = Auth::user();
+
+        return response()->json([
+            'message' => 'Login success.',
+            'data' => new UserResource($user)
+        ])->setStatusCode(200)->cookie(
+            'token', $token, 30
+        );
+    }
+
+    public function register(UserRegisterRequest $request){
+        $request->validated();
+
+        if (User::where([
+            ['username', $request['username']],
+            ['is_admin', true]
+        ])->count() == 1) {
+            throw new HttpResponseException(response([
+                'errors' => [
+                    'username' => [
+                            'This username is already taken.'
+                        ]
+                    ]
+            ], 400));
+        }
+
+
+        $user = User::create([
+            'username' => $request->username,
+            'password' => Hash::make($request->password),
+            'is_admin' => true
+        ]);
+
+        return response()->json([
+            'message' => 'Register success.',
+            'data' => new UserResource($user)
+        ])->setStatusCode(200);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json([
+            'message' => 'Logout success.',
+        ]);
+    }
+
+    public function refresh()
+    {
+        return response()->setStatusCode(200)->cookie(
+            'token', Auth::refresh(), 30
+        );;
+    }
+
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Model
+class User extends Authenticatable implements JWTSubject
 {
     protected $table = 'users';
     protected $primaryKey = 'id';
@@ -19,8 +20,23 @@ class User extends Model
         'refresh_token'
     ];
 
+    protected $hidden = [
+        'password',
+        'refresh_token',
+    ];
+
     public function products():HasMany
     {
         return $this->hasMany(Product::class, 'owned_by', 'id');
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
